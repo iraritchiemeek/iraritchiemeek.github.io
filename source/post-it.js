521a9b154698e2da14c98eb6266976476505e35b
iraritchiemeek <apprentice@StMac4.local>
2015-05-12 10:36:54 +1200

    Got adding div at position working

--- source/post-it.js
+++ source/post-it.js
@@ -1,19 +1,31 @@
 var Board = function( selector ) {
   // Your board related code goes here
-  
+
   // Use $elem to access the DOM element for this board
   var $elem = $( selector );
-  
+
   function initialize() {
-    // What needs to happen when this object is created?
+    $elem.on('click', function(e) {
+      console.log('test')
+      div = $("<div />")
+      div.attr({class: 'post-it'});
+      div.css({top: e.pageY, left: e.pageX})
+      div.append('<div class="header"></div>')
+      div.append('<div class="conent"></div>')
+      $elem.append(div)
+    });
   };
 
   initialize();
 };
 
 var PostIt = function() {
-  // Your post-it related code goes here
 };
+
+PostIt.prototype.addNote = function() {
+  console.log('work')
+}
+
 
 $(function() {
   // This code will run when the DOM has finished loading
